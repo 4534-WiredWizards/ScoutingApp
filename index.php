f56182b78e35d9fb20f17a766b69af264ae2bd26
Brandon Dyer <brandondyer64@gmail.com>
2016-01-08 19:38:57 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
 <body>
 	<h1>Scouting App 2016</h1>
 	<p>Sample Text</p>
+	<p><?php echo 'PHP'; ?></p>
+	<p id="js"></p>
+	<script type="text/javascript">
+	document.getElementById("js").innerHTML = "JavaScript";
+	</script>
 </body>
 </html>
