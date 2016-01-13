4382693944d00aa40ddd5df8cd14ab7605760ae1
Brandon Dyer <brandondyer64@gmail.com>
2016-01-12 19:18:38 -0500

    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

--- js/register.js
+++ js/register.js
@@ -2,21 +2,13 @@ $(document).ready(function() {
    $('#registerForm').on('submit', function() {
       // http://api.jquery.com/jquery.ajax/
       // api/register
-	  var xhttp;
-	  if (window.XMLHttpRequest) {
-	     xhttp = new XMLHttpRequest();
-	  } else {
-	     // code for IE6, IE5
-	     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	  }
-	  xhttp.onreadystatechange = function() {
-	     if (xhttp.readyState == 4 && xhttp.status == 200) {
-	        document.getElementById("response").innerHTML = xhttp.responseText;
-	        console.log("---SERVER---: " + xhttp.responseText);
-	     }
-	  };
-	  xhttp.open("POST", "api/pages/register.php", true);
-	  var sendString = document.getElementById("registerForm").serialize();
-	  xhttp.send(sendString);
+	  $.ajax({
+		 url: "api/pages/register.php",
+		 data: $('#registerForm').serialize(),
+		 method: "POST"
+      }).done(function(){
+         
+      });
+	  return false;
    });
 });
