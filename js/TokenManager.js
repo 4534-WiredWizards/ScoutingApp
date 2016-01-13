be17b16d58c35a4f4c9d57619f3dc20cba3f4913
Daniel Wilson <dwilson@quintify.com>
2016-01-13 10:33:22 -0500

    Moved token helper to seperate file, added requireSignin option for route helper

---
+++ js/TokenManager.js
@@ -0,0 +1,29 @@
+var TokenManager = (function() {
+   function TokenManager(ns) {
+      this.ns = ns || "";
+   }
+
+   TokenManager.prototype.get = function() {
+      return localStorage.getItem(this.ns+".token") || "";
+   }
+
+   TokenManager.prototype.set = function(token) {
+      return localStorage.setItem(this.ns+".token", token || "");
+   }
+
+   TokenManager.prototype.auth = function(data) {
+      var _this = this;
+
+      return $.ajax({
+         url: "api/auth",
+         data: data,
+         method: "POST"
+      }).then(function(res) {
+         if (res.success && res.token) {
+            _this.set(res.token);
+         }
+      });
+   }
+
+   return TokenManager;
+})();
