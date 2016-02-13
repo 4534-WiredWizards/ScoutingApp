36ea918117f698d4f0f9656438e4f3f143800f15
Daniel Wilson <dwilson@quintify.com>
2016-02-13 16:56:33 -0500

    Pagination\!

--- js/routes.js
+++ js/routes.js
@@ -14,7 +14,35 @@ var token = new TokenManager("ww-scouting", setLoggedin.bind(this, true), setLog
 var messages = new MessageManager(".alerts", []);
 
 // Initialize app url route manager
-var routes = new RoutesManager([], "", "team", token);
+var routes = new RoutesManager([], "", "teams", token);
+
+ractiveMethods = ({
+   setParam: (function(key, value) {
+      var params = getParams();
+      params[key] = value;
+      setHashParams(params);
+   }),
+   setParams: setHashParams,
+});
+
+function setHashParams(params) {
+   window.location.hash = window.location.hash.split("?").concat("")[0] + "?" + $.param(params);
+}
+
+function RactiveCustom(config, data, defaultParams) {
+   var config = $.extend({
+      el: ".main",
+      template: data.template
+   }, ractiveMethods, config);
+
+   ractive = new Ractive(config);
+
+   ractive.set({
+      params: getParams(defaultParams || {}, window.location.hash)
+   });
+
+   return ractive;
+}
 
 // Register app routes
 routes.register("/home", {
@@ -49,9 +77,7 @@ routes.register("/signin", {
 routes.register("/team/new", {
    template: "templates/team/form.html",
    init: function(data) {
-      ractive = new Ractive({
-         el: ".main",
-         template: data.template,
+      ractive = RactiveCustom({
          data: {
             action: "team/new",
             scores: {
@@ -74,7 +100,7 @@ routes.register("/team/new", {
                return JSON.stringify(this.get('questions'));
             },
          }
-      })
+      }, data);
       this.updateTitle("Add a Team");
    },
    formSuccess: function(res) {
@@ -96,9 +122,7 @@ routes.register("/team/:teamNum", {
       }
    },
    init: function(data, teamNum) {
-      ractive = new Ractive({
-         el: ".main",
-         template: data.template,
+      ractive = RactiveCustom({
          data: {
             splitLine: function(val) {
                return (val || "").split(/\s*\n\s*/g).filter(Boolean);
@@ -114,7 +138,7 @@ routes.register("/team/:teamNum", {
                return JSON.stringify(this.get('questions'));
             },
          }
-      });
+      }, data);
       delete data.team.scores_json;
       delete data.team.questions_json;
 
@@ -146,16 +170,14 @@ routes.register("/user/:userID", {
       }
    },
    init: function(data, userID) {
-      ractive = new Ractive({
-         el: ".main",
-         template: data.template,
+      ractive = RactiveCustom({
          data: {
             splitLine: function(val) {
                return (val || "").split(/\s*\n\s*/g).filter(Boolean);
             },
             score: 0
          }
-      })
+      }, data);
       ractive.set(data.user);
       this.updateTitle("{{firstname}} {{lastname}} ({{username}})", data.user);
    },
@@ -174,13 +196,11 @@ routes.register("/user/:userID/edit", {
       }
    },
    init: function(data, userID) {
-      ractive = new Ractive({
-         el: ".main",
-         template: data.template,
+      ractive = RactiveCustom({
          data: {
             action: "user/"+userID+"/edit"
          }
-      })
+      }, data);
       ractive.set(data.user);
       this.updateTitle("{{firstname}} {{lastname}} ({{username}})", data.user);
       $("textarea").each(function() {
@@ -203,9 +223,7 @@ routes.register("/team/:teamNum/edit", {
       }
    },
    init: function(data, teamNum) {
-      ractive = new Ractive({
-         el: ".main",
-         template: data.template,
+      ractive = RactiveCustom({
          data: {
             action: "team/"+teamNum+"/edit"
          },
@@ -217,7 +235,7 @@ routes.register("/team/:teamNum/edit", {
                return JSON.stringify(this.get('questions'));
             },
          }
-      })
+      }, data);
       delete data.team.scores_json;
       delete data.team.questions_json;
       ractive.set(data.team);
@@ -225,48 +243,71 @@ routes.register("/team/:teamNum/edit", {
    },
    requireSignin: true
 });
+
+function getParams(params, str) {
+   var str = str || window.location.hash;
+   str = str.split("?").concat(["",""])[1];
+   var params = typeof params == "object" ? params : {};
+   str
+      .replace(/(^\?)/,'')
+      .split("&")
+      .filter(Boolean)
+      .forEach(function(param) {
+         var param = param.split("=").concat("");
+         var value = param[1];
+         if (Number(value) == value) {
+            value = Number(value);
+         }
+         params[param[0]] = value;
+      });
+   return $.extend({}, params);
+}
+
+var teamParams = ({
+   page: 1,
+   limit: 10,
+   // q: ""
+});
 routes.register("/team", {
    template: "templates/team/list.html",
    dataCallbacks: {
-      teams: function(_this, callback) {
-         API.get("team", {}, function(res) {
+      teams: function(_this, callback, config) {
+         var params = getParams(teamParams);
+         API.get("team", params, function(res) {
+            res.data.numPages = res.numPages;
             callback(res.data);
          });
       }
    },
    init: function(data) {
-      var ractive = new Ractive({
-         el: ".main",
-         template: data.template,
-         data: {
-            teams: data.teams
-         }
-      });
+      ractive = RactiveCustom({
+         data: data
+      }, data, teamParams);
       this.updateTitle("Teams");
    },
    requireSignin: true
 });
+
 routes.register("/user", {
    template: "templates/user/list.html",
    dataCallbacks: {
       users: function(_this, callback) {
          API.get("user", {}, function(res) {
+            res.data.numPages = res.numPages;
+            console.log(res.data)
             callback(res.data);
          });
       }
    },
    init: function(data) {
-      var ractive = new Ractive({
-         el: ".main",
-         template: data.template,
-         data: {
-            users: data.users
-         }
-      });
+      ractive = RactiveCustom({
+         data: data
+      }, data);
       this.updateTitle("Users");
    },
    requireSignin: true
 });
+
 routes.register("/team/search/:query", {
    template: "templates/team/list.html",
    dataCallbacks: {
@@ -277,14 +318,12 @@ routes.register("/team/search/:query", {
       }
    },
    init: function(data, query) {
-      var ractive = new Ractive({
-         el: ".main",
-         template: data.template,
+      ractive = RactiveCustom({
          data: {
             teams: data.teams,
             query: query
          }
-      });
+      }, data);
       this.updateTitle("Teams");
    },
    requireSignin: true
@@ -298,4 +337,38 @@ routes.register("/not-found", {
       clearInterval(this.interval);
    },
    requireSignin: false
+});
+
+routes.register("/tba", {
+   template: "templates/tba.html",
+   init: function(data) {
+      ractive = RactiveCustom({
+         data: {
+            rows: [],
+            displayField: function(field) {
+               return field.replace(/[_]/g, " ").replace(/\s{2,}/g, " ").split(" ").map(function(word) {
+                  return word.charAt(0).toUpperCase() + word.slice(1);
+               }).join(" ");
+            }
+         },
+      }, data);
+      ractive.on("submit", function() {
+         var _this = this;
+         this.set("rows", []);
+         $.ajax({
+            dataType: "json",
+            url: "https://www.thebluealliance.com/api/v2/" + this.get("url"),
+            data: "",
+            headers: {
+               'X-TBA-App-Id': 'frc4534:scouting-app:testing'
+            }
+         }).then(function(res) {
+            if (!res.length) {
+               var res = [res];
+            }
+            _this.set("rows", res);
+         });
+      });
+      this.updateTitle("TBA");
+   }
 });
